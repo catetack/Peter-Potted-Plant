2784d26591f8d0ca8743a9282b99f0a4ea8e42cd
Mathieu Forster <MATHIEUFORSTER@cunet.carleton.ca>
2025-09-17 16:08:47 -0400

    Reformatted Controller Mapping

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -4,9 +4,9 @@ using UnityEngine.UI;
 public class playerController : MonoBehaviour
 {
     public GameObject player;
-    public float speed = 0f;
 
     InputSystem_Actions inputActions; // variable reference but there is no object in memory
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,17 +17,11 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         //displays the read value for the keyboard
-
-        //Vector2 translate = new Vector2(inputActions.Player.Head.ReadValue<Vector3>().x, 0);
-        //Debug.Log(inputActions.Player.Legs.ReadValue<Vector2>());
-        //inputActions.Player.Head.ReadValue<Vector2>();
         Debug.Log("LEGS: " + inputActions.Player.Legs.ReadValue<Vector2>() + ", " +
         inputActions.Player.Jump.ReadValue<float>() + System.Environment.NewLine +
         "HEAD: " + inputActions.Player.Head.ReadValue<Vector2>() + ", " +
         inputActions.Player.Burst.ReadValue<float>());
-
-
-        //player.transform.Translate(speed * translate);
     }
 }
