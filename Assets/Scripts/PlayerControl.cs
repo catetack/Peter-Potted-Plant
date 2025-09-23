c8923f25f0c78105a81429e4d96f803964231547
HDisHERE <125623145+HDisHERE@users.noreply.github.com>
2025-09-23 17:08:03 -0400

    Controller trigger added

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
+    private InputSystem_Actions  inputAction;
     Rigidbody2D rb;
 
     [Range(1,10)]
     public float jumpSpeed = 5.0f;
 
     private bool jumpPressed;
+
+    private bool jumpHold;
 
     public bool isGrounded;
 
@@ -20,6 +24,8 @@ public class PlayerControl : MonoBehaviour
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        inputAction = new InputSystem_Actions();
+        inputAction.Enable();
 
     }
 
@@ -28,17 +34,22 @@ public class PlayerControl : MonoBehaviour
     {
         //Jump
         jumpInput();
-        jump();
     }
 
     private void FixedUpdate()//Physics
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position,0.1f,ground);
+        onGround();
+        jump();
+    }
+
+    private void OnDisable()
+    {
+        inputAction.Disable();
     }
 
     private void jumpInput()
     {
-        jumpPressed = Input.GetButtonDown("Jump");
+        jumpPressed = inputAction.Player.Jump.IsPressed();
     }
 
     private void jump() 
@@ -47,5 +58,10 @@ public class PlayerControl : MonoBehaviour
         {
             rb.linearVelocity=Vector2.up*jumpSpeed;
         }
+    }
+
+    private void onGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
     }
 }
