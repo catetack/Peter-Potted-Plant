eadb0f6d267514d635c058f690795f0f02fafd71
nomba38 <noe38concepts@gmail.com>
2025-11-11 12:43:03 -0500

    animation fixes

--- Assets/Scripts/peter/playerStateManager.cs
+++ Assets/Scripts/peter/playerStateManager.cs
@@ -7,6 +7,7 @@ public class playerStateManager : MonoBehaviour
     public bool isDowned = false;
     public bool isReviving = false;
     public bool isHeavy = false;
+
     public float health = 100.0f;
     public float arduinoWaterValue;
 
@@ -29,6 +30,7 @@ public class playerStateManager : MonoBehaviour
         peterHead = GameObject.Find("peterHead");
         isHeavy = false;
         peterAnimator = GetComponentInChildren<Animator>();
+        peterAnimator.speed = 1; //make sure peter starts off idle
     }
 
     // Update is called once per frame
@@ -40,19 +42,25 @@ public class playerStateManager : MonoBehaviour
         //sets animation value to Peter's speed
         peterAnimator.SetFloat("Displacement Speed", displacementSpeed);
 
-        //if peter is moving forward, change his animation speed to match how fast he is moving
-        if (peterAnimator.GetFloat("Displacement Speed") >= 0.1)
+        ///if Peter is still, play the idle animation
+        if (peterAnimator.GetFloat("Displacement Speed") <= 0.1 && peterAnimator.GetFloat("Displacement Speed") >= -0.1)
+        {
+            peterAnimator.speed = 1;
+            
+        }
+
+        //if Peter is moving forward, change his animation speed to match how fast he is moving
+        else if (peterAnimator.GetFloat("Displacement Speed") > 0.1)
         {
             peterAnimator.speed = 1 + displacementSpeed;
         }
 
-        //if peter is moving backward, change his animation speed to match how fast he is moving
-        else if (peterAnimator.GetFloat("Displacement Speed") <= -0.1)
+        else
         {
             peterAnimator.speed = 1 + Math.Abs(displacementSpeed);
         }
 
-        //UnityEngine.Debug.Log("displacement speed is " + peterAnimator.GetFloat("Displacement Speed"));
-        //UnityEngine.Debug.Log("animation speed is " + peterAnimator.speed);
+        UnityEngine.Debug.Log("displacement speed is " + peterAnimator.GetFloat("Displacement Speed"));
+        UnityEngine.Debug.Log("animation speed is " + peterAnimator.speed);
     }
 }
