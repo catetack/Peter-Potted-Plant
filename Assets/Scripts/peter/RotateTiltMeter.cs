d1806c993a33a6047f6c72e115a84cdb97a9c95f
Mathieu Forster <119619910+catetack@users.noreply.github.com>
2025-11-25 23:34:49 -0500

    meter is glitchy

---
+++ Assets/Scripts/peter/RotateTiltMeter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class RotateTiltMeter : MonoBehaviour
+{
+    playerStateManager PlayerState;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        assignObjects();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, -30.0f + 60.0f * PlayerState.rotationRatio);
+        //PlayerState.rotationRatio;
+    }
+
+    void assignObjects()
+    {
+        PlayerState = GetComponentInParent<playerStateManager>();
+        if (PlayerState == null)
+        {
+            Debug.LogError("playerStateManager script not found on 'PlayerState' parent.");
+        }
+    }
+}
