aee70d88c423aa411c2bd95f19d5d19b4714616e
Mathieu Forster <119619910+catetack@users.noreply.github.com>
2025-11-26 11:02:33 -0500

    Tilt meter now works

---
+++ Assets/Scripts/GameUI/TiltMeterAdjustments.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TiltMeterAdjustments : MonoBehaviour
+{   
+    float ROTATION_SCALE = 100.0f;
+    float rotationScale;
+    float rotationAmount;
+    playerStateManager PlayerState;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        assignObjects();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationAmount = -rotationScale * PlayerState.rotationRatio;
+        if (rotationAmount < 0)
+        {
+            rotationAmount = rotationAmount + rotationScale;
+        }
+        else    
+        {
+            rotationAmount = rotationAmount - rotationScale;
+        }
+
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationAmount);
+        Debug.Log(rotationAmount);
+    }
+
+    void assignObjects()
+    {
+        rotationScale = ROTATION_SCALE;
+        PlayerState = GetComponentInParent<playerStateManager>();
+        if (PlayerState == null)
+        {
+            Debug.LogError("playerStateManager script not found on 'PlayerState' parent.");
+        }
+    }
+}
