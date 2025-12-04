fb0bb2739bb78387998b52a528d8c6614de78300
nomba38 <noe38concepts@gmail.com>
2025-12-04 10:40:03 -0500

    Update Spring.cs

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -3,22 +3,31 @@ using UnityEngine;
 public class Spring : MonoBehaviour
 {
     public float jumpForce = 10f;
+
+    //Sound
+    private AudioSource jumpSound;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        assignObjects();
+        jumpSound.Stop(); //so sound doesn't play on startup
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag== "Player")
         {
-            //Animation scripts here
-
+            jumpSound.Play(); //play sound on collision with jump pad
             //Function scripts
             collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity=new Vector2 (collision.gameObject.GetComponent<Rigidbody2D>().linearVelocityX, 0);
             //collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,jumpForce),ForceMode2D.Impulse);
             collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity=Vector2.up*jumpForce;
         }
+    }
+
+    void assignObjects()
+    {
+        jumpSound = GetComponent<AudioSource>();
     }
 }
